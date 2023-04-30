3d57e5624c87e4bd5f5d27495b115212e0feb11a
Danilo Acosta <danilo.acostadote@chasacademy.se>
2023-04-30 03:45:30 +0200

    Altered models

--- Models/PersonMovie.cs
+++ Models/PersonMovie.cs
@@ -2,16 +2,17 @@
 {
     public class PersonMovie
     {
-        public int Id { get; set; }
-        public int PersonId { get; set; }
-        public int GenreId { get; set; }
-        public int MovieId { get; set; }
+        public int PersonMovieId { get; set; }
+
+        public Person Person { get; set; } //This creates Foreignkey Person_PersonId in in the PersonMovie table
+
+        public Genre Genre { get; set; } //Foreignkey to GenreId
+
+        public Movie Movie { get; set; } //Foreignkey to MovieId
+
         public int Rating { get; set; }
+
         public string? Link { get; set; }
 
-
-        public virtual List<Person> Persons { get; set; }
-        public virtual List<Genre>  Genres { get; set; }
-        public virtual List<Movie> Movies { get; set;}
     }
 }
