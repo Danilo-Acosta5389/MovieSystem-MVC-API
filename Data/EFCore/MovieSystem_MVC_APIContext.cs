b382e44551be7d4c661134e289c64cee14d09f89
Danilo Acosta <danilo.acostadote@chasacademy.se>
2023-05-01 02:19:25 +0200

    Added concrete class, changed context and moved controller

--- Data/EFCore/MovieSystem_MVC_APIContext.cs
+++ Data/EFCore/MovieSystem_MVC_APIContext.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using MovieSystem_MVC_API.Models;
 
-namespace MovieSystem_MVC_API.Models
+namespace MovieSystem_MVC_API.Data.EFCore
 {
     public class MovieSystem_MVC_APIContext : DbContext
     {
-        public MovieSystem_MVC_APIContext (DbContextOptions<MovieSystem_MVC_APIContext> options)
+        public MovieSystem_MVC_APIContext(DbContextOptions<MovieSystem_MVC_APIContext> options)
             : base(options)
         {
         }
 
-        public DbSet<MovieSystem_MVC_API.Models.Person> Person { get; set; } = default!;
+        public DbSet<Person> Person { get; set; } = default!;
     }
 }
