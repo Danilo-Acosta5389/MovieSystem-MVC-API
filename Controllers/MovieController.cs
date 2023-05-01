69cec54c3caa570f3502d819e552da7d9b03d1b6
Danilo Acosta <danilo.acostadote@chasacademy.se>
2023-05-01 04:18:04 +0200

    Added Movie model and did migration

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieSystem_MVC_API.Models;
+using MovieSystem_MVC_API.Data.EFCore;
+
+namespace MovieSystem_MVC_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : MovieSystem_MVC_APIController<Movie, EfCoreMovieRepository>
+    {
+        public MovieController(EfCoreMovieRepository repository) : base(repository)
+        {
+
+        }
+    }
+}
--- Migrations/20230501021435_Movie.Designer.cs
+++ Migrations/20230501021435_Movie.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MovieSystem_MVC_API.Data.EFCore;
 
@@ -11,9 +12,10 @@ using MovieSystem_MVC_API.Data.EFCore;
 namespace MovieSystem_MVC_API.Migrations
 {
     [DbContext(typeof(MovieSystem_MVC_APIContext))]
-    partial class MovieSystem_MVC_APIContextModelSnapshot : ModelSnapshot
+    [Migration("20230501021435_Movie")]
+    partial class Movie
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
